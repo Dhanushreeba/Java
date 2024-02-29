f294860134b28b258038c1b952a255b61e1a0e7a
Dhanushreeba <dhanushreeba.xworkz@gmail.comgit config --global user.name Dhanushreeba git config --global user.email dhanushreeba.xworkz@gmail.com>
2024-02-29 20:15:32 +0530

    Pattern program

--- Variable/src/com/xworkz/Tree/Pattern8.java
+++ Variable/src/com/xworkz/Tree/Pattern8.java
@@ -1,24 +1,31 @@
 package com.xworkz.Tree;
 
-public class Pattern6 {
+public class Pattern8 {
 
 	public static void main(String[] args) {
-
+		
 		int n=5;
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=i;j++) {
+		for(int i=1;i<n;i++) {
+			for(int j=i;j<=n;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
 				System.out.print("* ");
 			}
 			
 			System.out.println();
 		}
 		for(int i=1;i<=n;i++) {
-			for(int j=i;j<n;j++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=i;k<=n;k++) {
 				System.out.print("* ");
 			}
 			
 			System.out.println();
-	}
+		}
+
 	}
 
 }
