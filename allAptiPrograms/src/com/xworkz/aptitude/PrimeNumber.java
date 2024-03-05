2afcca56147fa8866f54814bbaaf3e458daa17fc
Dhanushreeba <dhanushreeba.xworkz@gmail.comgit config --global user.name Dhanushreeba git config --global user.email dhanushreeba.xworkz@gmail.com>
2024-03-05 10:12:14 +0530

    Prime Number

--- allAptiPrograms/src/com/xworkz/aptitude/PrimeNumber.java
+++ allAptiPrograms/src/com/xworkz/aptitude/PrimeNumber.java
@@ -1,8 +1,9 @@
-package com.xworkz.Tree;
+package com.xworkz.aptitude;
+
 
 public class PrimeNumber {
-
-	  static int num[]= {1,4,3,8,98,5,7};
+	
+	static int num[]= {1,4,3,8,98,5,7};
 	  static int temp;
 	  
 	public static void main(String[] args) {
