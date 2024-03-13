dc3081aaef6a100fe66377829a66bed2df16df27
Dhanushreeba <dhanushreeba.xworkz@gmail.comgit config --global user.name Dhanushreeba git config --global user.email dhanushreeba.xworkz@gmail.com>
2024-03-13 19:17:09 +0530

    PrimeNumber program

--- allAptiPrograms/src/com/xworkz/aptitude/PrimeNumber.java
+++ allAptiPrograms/src/com/xworkz/aptitude/PrimeNumber.java
@@ -3,19 +3,31 @@ package com.xworkz.aptitude;
 
 public class PrimeNumber {
 	
-	static int num[]= {1,4,3,8,98,5,7};
-	  static int temp;
+	public static void main(String[] args)
+	{
+		//ex:7 = 1,2,3,4,5,6,7
+		int num=3;
+		int count=0;
+		for(int i=1;i<=num;i++)
+		{
+			if(num%i==0) //7/1=0
+			{
+				count++;
+			}
+		}
+       if(count==2)
+       {
+    	   System.out.println(num +" is prime number");
+       }
+       else
+       {
+    	   System.out.println("not a prime number");
+       }
+}
+
+
 	  
-	public static void main(String[] args) {
-		
-		if(temp %= num[]) {
-			System.out.println("prime number");
-		}
-		else {
-			
-			System.out.println("not prime number");
-		}
 
 	}
-
-}
+	
+	 
