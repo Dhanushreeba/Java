9970e97b95ba47830acd23658c43ce0b3a6b68ad
Dhanushreeba <dhanushreeba.xworkz@gmail.comgit config --global user.name Dhanushreeba git config --global user.email dhanushreeba.xworkz@gmail.com>
2024-03-13 19:45:21 +0530

    Thread

---
+++ threads/src/com/xworkz/thread/Dog.java
@@ -0,0 +1,14 @@
+package com.xworkz.thread;
+
+	public class Dog extends Thread
+	{
+		@Override
+		public void run()
+		{
+			for(int i=0;i<=5;i++)
+			{
+	    System.out.println("hi  "+ Thread.currentThread().getName()+"and "+Thread.currentThread().getId());
+			}
+		}
+	
+}
